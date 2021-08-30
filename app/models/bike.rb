5b3cc2afbd35fb6cb12ef34ef1085a365c2bfbe9
Johanna Brewer <johannab@gmail.com>
2021-08-30 14:37:24 -0400

    Adds basic models for Stations and Bikes.

---
+++ app/models/bike.rb
@@ -0,0 +1,7 @@
+class Bike < ApplicationRecord
+  validates_presence_of    :identifier
+  validates_uniqueness_of  :identifier
+  
+  belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
+
+end
