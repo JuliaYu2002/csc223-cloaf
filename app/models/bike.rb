ad2e60f50f329141dc482c5cf52c9f4247a04506
EmmaConnolly <emconnolly72@gmail.com>
2022-03-26 14:28:48 -0400

    DO NOT MERGE needs testing. Added scopes and validations to User and an association between User and Bike.

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,8 @@
 class Bike < ApplicationRecord
   validates_presence_of    :identifier
   validates_uniqueness_of  :identifier
-  
+
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
+  belongs_to :user, optional: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,31 @@
 class User < ApplicationRecord
+  validates_presence_of   :email
+  validates_uniqueness_of :username
+                          :email
+  validate                :has_username
+                          :has_password
+
+  has_many :bikes, optional:true
+
+  # I know we're only allowing one bike per user to start with,
+  # but I defined it as a one-to-many relationship to make it easier to change later.
+
+  scope :registered, lambda { where("registration_date != ?", nil)  }
+
+  private
+
+    def has_username
+      if :registered && :username == nil
+        errors.add(:username, "Must enter a username.")
+      end
+    end
+
+  private
+
+    def has_password
+      if :registered && :password == nil
+        errors.add(:password, "Must enter a password.")
+      end
+    end
+
 end
