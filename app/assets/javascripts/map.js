73a2f52c468e22a6fe9093dc9a6d1dd0c80bb60a
Allison Wong <allisonjingwong@gmail.com>
2022-04-02 12:03:25 -0400

    Partially working map (empty box), missing tiles

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,7 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
